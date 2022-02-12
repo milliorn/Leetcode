df16005202052934b5cd66ce02b33c5c9d607fba
Scott Milliorn <scottmilliorn@gmail.com>
2022-02-12 09:37:12 -0800

    java kt

--- Easy/Roman to Integer/src/Solution.java
+++ Easy/Roman to Integer/src/Solution.java
@@ -1,36 +1,39 @@
-import java.util.HashMap
+import java.util.HashMap;
+import java.util.Map;
 
-class Solution {
-    fun romanToInt(s: String): Int {
-        val map = roman()
-        val length = s.length
-        var result = map[s[length - 1]]!!
-        for (i in length - 2 downTo 0) {
-            result = getResult(s, map, result, i)
-        }
-        return result
+public class Solution {
+
+  public int romanToInt(String s) {
+    Map<Character, Integer> map = roman();
+    int length = s.length();
+    int result = map.get(s.charAt(length - 1));
+
+    for (int i = length - 2; i >= 0; i--) {
+      result = getResult(s, map, result, i);
     }
 
-    private fun getResult(s: String, map: Map<Char, Int>, result: Int, i: Int): Int {
-        var result = result
-        val temp = s[i]
-        if (map[temp]!! >= map[s[i + 1]]!!) {
-            result += map[temp]!!
-        } else {
-            result -= map[temp]!!
-        }
-        return result
-    }
+    return result;
+  }
 
-    private fun roman(): Map<Char, Int> {
-        val map: MutableMap<Char, Int> = HashMap()
-        map['I'] = 1
-        map['V'] = 5
-        map['X'] = 10
-        map['L'] = 50
-        map['C'] = 100
-        map['D'] = 500
-        map['M'] = 1000
-        return map
+  private int getResult(String s, Map<Character, Integer> map, int result, int i) {
+    char temp = s.charAt(i);
+    if (map.get(temp) >= map.get(s.charAt(i + 1))) {
+      result += map.get(temp);
+    } else {
+      result -= map.get(temp);
     }
+    return result;
+  }
+
+  private Map<Character, Integer> roman() {
+    Map<Character, Integer> map = new HashMap<>();
+    map.put('I', 1);
+    map.put('V', 5);
+    map.put('X', 10);
+    map.put('L', 50);
+    map.put('C', 100);
+    map.put('D', 500);
+    map.put('M', 1000);
+    return map;
+  }
 }
